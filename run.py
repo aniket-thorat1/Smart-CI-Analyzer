1dc25a2258955e162c9d139239ec08a83939d250
aniket-thorat <aniketsthorat11@gmail.com>
2026-06-09 14:01:06 +0530

    updated code for deployment

--- run.py
+++ run.py
@@ -4,8 +4,9 @@ from app.config import Config
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=Config.PORT,
-        debug=True
-    )
+    # app.run(
+    #     host="0.0.0.0",
+    #     port=Config.PORT,
+    #     debug=True
+    # )
+    app.run()
